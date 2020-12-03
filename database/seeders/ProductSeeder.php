05b67b87dd97f8fc4c72f373d4ce88b88cfe3039
Suneth Perera <sunethjoes@gmail.com>
2020-12-03 18:04:07 +1100

    seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -68,5 +68,58 @@ class ProductSeeder extends Seeder
                 'Product02.jpg',
                 new DateTime()
             ]);
+        DB::insert(
+            'insert into products (name,description,color,weight,price,cost,stock,imageurl,created_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                'Stripe Web Pin Buckle Belt',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Navy,white',
+                0.50,
+                15.00,
+                07.00,
+                20,
+                'Product03.jpg',
+                new DateTime()
+            ]);
+        DB::insert(
+            'insert into products (name,description,color,weight,price,cost,stock,imageurl,created_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                'Mens Court Shoes',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Navy,Tan',
+                2.00,
+                20.00,
+                10.00,
+                15,
+                'Product04.jpg',
+                new DateTime()
+            ]);
+        DB::insert(
+            'insert into products (name,description,color,weight,price,cost,stock,imageurl,created_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                'Quilted Romper',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Oatmeal A',
+                1.00,
+                10.00,
+                06.00,
+                18,
+                'Product05.jpg',
+                new DateTime()
+            ]);
+        DB::insert(
+            'insert into products (name,description,color,weight,price,cost,stock,imageurl,created_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                'Pinafore Dress',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Cornsilk',
+                1.00,
+                15.00,
+                08.00,
+                25,
+                'Product06.jpg',
+                new DateTime()
+            ]);
+
     }
 }
